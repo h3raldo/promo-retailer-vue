8a3dfa39bf432c1a3fd5e4224cca9ae341eeb5ac
h3raldo <me@heraldo.io>
2024-04-05 23:44:43 -0500

    Basic refactoring to prepare quote and order share codebase

---
+++ src/js/entity/Order.js
@@ -0,0 +1,58 @@
+import OrderItem from "@/js/entity/Order/Order.Item.js";
+import OrderFee from "@/js/entity/Order/Order.Fee.js";
+import OrderLogo from "@/js/entity/Order/Order.Logo.js";
+import OrderVendor from "@/js/entity/Order/Order.Vendor.js";
+
+function create(){
+    let date = new Date();
+    return {
+        id: id,
+        info: {
+            title: 'New Quote',
+            status: 'draft',
+            category: 'standard',
+            notes: {
+                public: '',
+                private: '',
+                flags: []
+            },
+            date: date.toISOString().split('T')[0],
+            deliver_by: date.toISOString().split('T')[0],
+            deliver_by_strict: false
+        },
+        client: {
+            name: 'New Client',
+            email: '',
+            info: '',
+            id: '',
+            ship_to: '',
+        },
+        items: [],
+        fees: [],
+        totals: {
+            total: 0,
+            cost: 0,
+            margin: 0,
+            qty: 0,
+        }
+    }
+}
+
+function patchData( init )
+{
+    let order = init.order;
+
+    order.id = init.id;
+    order.info.status = init.status;
+
+    return order;
+}
+
+export default {
+    create: create,
+    patchData: patchData,
+    item: OrderItem,
+    fee: OrderFee,
+    logo: OrderLogo,
+    vendor: OrderVendor
+}
